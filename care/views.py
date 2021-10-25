d4f3f555a7fe8a2a9284ebbd1fe09c4cc915fd78
Abbasion <82872796+Abbasion@users.noreply.github.com>
2021-10-25 04:57:43 -0700

    first commit

---
+++ care/views.py
@@ -0,0 +1,294 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'title': 'Home page title'})
+
+def services(request):
+    return render(request, 'services.html', {'title': 'Home page title'})
+
+def gallery(request):
+    return render(request, 'gallery.html', {'title': 'Home page title'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'Home page title'})
+
+def contact(request):
+    return render(request, 'contact.html', {'title': 'Home page title'})
+
+def jobs(request):
+    return render(request, 'jobs.html', {'title': 'Home page title'})
+
+
+def NusrsingServices(request):
+    return render(request, 'NusrsingServices.html', {'title': 'Home page title'})
+
+def rehabilitationservicesinclude(request):
+    return render(request, 'rehabilitationservicesinclude.html', {'title': 'Home page title'})
+
+def nursinghomecriticalcarespecified(request):
+    return render(request, 'nursinghomecriticalcarespecified.html', {'title': 'Home page title'})
+
+def homeoncallhealthaides24hrs(request):
+    return render(request, '24hrshomeoncallhealthaides.html', {'title': 'Home page title'})
+
+def homediagnosticlabandbloodtransfusionservices(request):
+    return render(request, 'homediagnosticlabandbloodtransfusionservices.html', {'title': 'Home page title'})
+
+def homemedicalequipment(request):
+    return render(request, 'homemedicalequipment.html', {'title': 'Home page title'})
+
+def basicattendantnursingcare(request):
+    return render(request, 'basicattendantnursingcare.html', {'title': 'Home page title'})
+
+def assistantnursingcare(request):
+    return render(request, 'assistantnursingcare.html', {'title': 'Home page title'})
+
+def qualifiedregisterednursingcare(request):
+    return render(request, 'qualifiedregisterednursingcare.html', {'title': 'Home page title'})
+
+def specializednursingcare(request):
+    return render(request, 'specializednursingcare.html', {'title': 'Home page title'})
+
+def postsurgicalnursingcare(request):
+    return render(request, 'postsurgicalnursingcare.html', {'title': 'Home page title'}) 
+# shuru 
+
+def poststrokenursingcare(request):
+    return render(request, 'poststrokenursingcare.html', {'title': 'Home page title'}) 
+
+def maternitynursingcare(request):
+    return render(request, 'maternitynursingcare.html', {'title': 'Home page title'}) 
+
+def neonatalnursingcare(request):
+    return render(request, 'neonatalnursingcare.html', {'title': 'Home page title'}) 
+
+def geriatricnursingcare(request):
+    return render(request, 'geriatricnursingcare.html', {'title': 'Home page title'}) 
+
+def medicaloncologynursingcare(request):
+    return render(request, 'medicaloncologynursingcare.html', {'title': 'Home page title'}) 
+
+def diabeticfootnursingcare(request):
+    return render(request, 'diabeticfootnursingcare.html', {'title': 'Home page title'}) 
+
+def Childdaycarenursingcare(request):
+    return render(request, 'Childdaycarenursingcare.html', {'title': 'Home page title'}) 
+
+def physicaltherapy(request):
+    return render(request, 'physicaltherapy.html', {'title': 'Home page title'})    
+
+def speechtherapy(request):
+    return render(request, 'speechtherapy.html', {'title': 'Home page title'})
+
+def psyhotherapy(request):
+    return render(request, 'psyhotherapy.html', {'title': 'Home page title'})
+
+def respiratorytherapy(request):
+    return render(request, 'respiratorytherapy.html', {'title': 'Home page title'})
+
+def occupationaltherapy(request):
+    return render(request, 'occupationaltherapy.html', {'title': 'Home page title'})
+
+def cuppingtherapy_hijama(request):
+    return render(request, 'cuppingtherapy_hijama.html', {'title': 'Home page title'})
+
+def ECMtherapy(request):
+    return render(request, 'ECMtherapy.html', {'title': 'Home page title'})
+
+def massagetherapy(request):
+    return render(request, 'massagetherapy.html', {'title': 'Home page title'})
+
+def diabeticfoottherapy(request):
+    return render(request, 'diabeticfoottherapy.html', {'title': 'Home page title'})
+
+def orthotics_prosthetics(request):
+    return render(request, 'orthotics_prosthetics.html', {'title': 'Home page title'})
+
+def diabeticcare(request):
+    return render(request, 'diabeticcare.html', {'title': 'Home page title'})
+
+def cardiaccare(request):
+    return render(request, 'cardiaccare.html', {'title': 'Home page title'})
+
+def maintainingintake(request):
+    return render(request, 'maintainingintake.html', {'title': 'Home page title'})
+
+def intravenoustherapy(request):
+    return render(request, 'intravenoustherapy.html', {'title': 'Home page title'})
+
+def untramuscularinjection(request):
+    return render(request, 'untramuscularinjection.html', {'title': 'Home page title'})
+
+def woundcareandprevention(request):
+    return render(request, 'woundcareandprevention.html', {'title': 'Home page title'})
+
+def bedsorescare(request):
+    return render(request, 'bedsorescare.html', {'title': 'Home page title'})
+
+def completetracheostomycare(request):
+    return render(request, 'completetracheostomycare.html', {'title': 'Home page title'})
+
+def COPDasthmaManagement(request):
+    return render(request, 'COPDasthmaManagement.html', {'title': 'Home page title'})
+
+def ora1medicationmanagement(request):
+    return render(request, 'ora1medicationmanagement.html', {'title': 'Home page title'})
+
+def onewgtubecare(request):
+    return render(request, 'onewgtubecare.html', {'title': 'Home page title'})
+
+def PEGTubeCare(request):
+    return render(request, 'PEGTubeCare.html', {'title': 'Home page title'})
+
+def foleyscathetercare(request):
+    return render(request, 'foleyscathetercare.html', {'title': 'Home page title'})
+
+def ColostomyDrainageBagCare(request):
+    return render(request, 'ColostomyDrainageBagCare.html', {'title': 'Home page title'})
+
+def ColostomyDrainageBagCare(request):
+    return render(request, 'ColostomyDrainageBagCare.html', {'title': 'Home page title'})
+
+def nebulizerandsteamtherapy(request):
+    return render(request, 'nebulizerandsteamtherapy.html', {'title': 'Home page title'})
+
+def pulseoximetry(request):
+    return render(request, 'pulseoximetry.html', {'title': 'Home page title'})
+
+def diaperchanging(request):
+    return render(request, 'diaperchanging.html', {'title': 'Home page title'})
+
+def AssistwithBathing(request):
+    return render(request, 'AssistwithBathing.html', {'title': 'Home page title'})
+
+def perrectalevacuation(request):
+    return render(request, 'perrectalevacuation.html', {'title': 'Home page title'})
+
+def minorsurgicalprocedure(request):
+    return render(request, 'minorsurgicalprocedure.html', {'title': 'Home page title'})
+
+def Homecollectionofbloodsamples(request):
+    return render(request, 'Homecollectionofbloodsamples.html', {'title': 'Home page title'})
+
+def homeholtermonitor(request):
+    return render(request, 'homeholtermonitor.html', {'title': 'Home page title'})
+
+def ultrasoundathome(request):
+    return render(request, 'ultrasoundathome.html', {'title': 'Home page title'})
+
+def pelviscare(request):
+    return render(request, 'pelviscare.html', {'title': 'Home page title'})
+
+def fetalcare(request):
+    return render(request, 'fetalcare.html', {'title': 'Home page title'})
+
+def pregnancyusg(request):
+    return render(request, 'pregnancyusg.html', {'title': 'Home page title'})
+
+def ECGathome(request):
+    return render(request, 'ECGathome.html', {'title': 'Home page title'})
+
+def ECHOathome(request):
+    return render(request, 'ECHOathome.html', {'title': 'Home page title'})
+
+def vaccination(request):
+    return render(request, 'vaccination.html', {'title': 'Home page title'})
+
+def bloodtransfusion(request):
+    return render(request, 'bloodtransfusion.html', {'title': 'Home page title'})
+
+def bloodtransfusion(request):
+    return render(request, 'bloodtransfusion.html', {'title': 'Home page title'})
+
+def BloodDonorsArrangement(request):
+    return render(request, 'BloodDonorsArrangement.html', {'title': 'Home page title'})
+
+def OxygenatedAmbulanceServices(request):
+    return render(request, 'OxygenatedAmbulanceServices.html', {'title': 'Home page title'})
+
+def deliveryofreports(request):
+    return render(request, 'deliveryofreports.html', {'title': 'Home page title'})
+
+def patientsbeds(request):
+    return render(request, 'patientsbeds.html', {'title': 'Home page title'})
+
+def cardiacmonitors(request):
+    return render(request, 'cardiacmonitors.html', {'title': 'Home page title'})
+
+def suctionmachines(request):
+    return render(request, 'suctionmachines.html', {'title': 'Home page title'})
+
+def BipapMachines(request):
+    return render(request, 'BipapMachines.html', {'title': 'Home page title'})
+
+def WheelChairs(request):
+    return render(request, 'WheelChairs.html', {'title': 'Home page title'})
+
+def Electricwheelchairs(request):
+    return render(request, 'Electricwheelchairs.html', {'title': 'Home page title'})
+
+def Oxygencylinders(request):
+    return render(request, 'Oxygencylinders.html', {'title': 'Home page title'})
+
+def Glucometers(request):
+    return render(request, 'Glucometers.html', {'title': 'Home page title'})
+
+def PulseOximeter(request):
+    return render(request, 'PulseOximeter.html', {'title': 'Home page title'})
+
+def Spirometers(request):
+    return render(request, 'Spirometers.html', {'title': 'Home page title'})
+
+def BloodPressureApparatus(request):
+    return render(request, 'BloodPressureApparatus.html', {'title': 'Home page title'})
+
+def Nebulizer(request):
+    return render(request, 'Nebulizer.html', {'title': 'Home page title'})
+
+def FirstAidBoxes(request):
+    return render(request, 'FirstAidBoxes.html', {'title': 'Home page title'})
+
+def InfraredLights(request):
+    return render(request, 'InfraredLights.html', {'title': 'Home page title'})
+
+def BedsTables(request):
+    return render(request, 'BedsTables.html', {'title': 'Home page title'})
+
+def onevStands(request):
+    return render(request, 'onevStands.html', {'title': 'Home page title'})
+
+def AirMattress(request):
+    return render(request, 'AirMattress.html', {'title': 'Home page title'})
+
+def PatientsLifts(request):
+    return render(request, 'PatientsLifts.html', {'title': 'Home page title'})
+
+def RollatorsWalkers(request):
+    return render(request, 'RollatorsWalkers.html', {'title': 'Home page title'})
+
+def Crutches(request):
+    return render(request, 'Crutches.html', {'title': 'Home page title'})
+
+def AllSizesDiapers(request):
+    return render(request, 'AllSizesDiapers.html', {'title': 'Home page title'})
+
+def AllSizesCatheters(request):
+    return render(request, 'AllSizesCatheters.html', {'title': 'Home page title'})
+
+def CottonRolls(request):
+    return render(request, 'CottonRolls.html', {'title': 'Home page title'})
+
+def BedPansUrinals(request):
+    return render(request, 'BedPansUrinals.html', {'title': 'Home page title'})
+
+def DisposableGloves(request):
+    return render(request, 'DisposableGloves.html', {'title': 'Home page title'})
+
+def SterilizedGauzes(request):
+    return render(request, 'SterilizedGauzes.html', {'title': 'Home page title'})
+
+def oneVinfusions(request):
+    return render(request, 'oneVinfusions.html', {'title': 'Home page title'})
+
